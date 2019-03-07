9cb871ab32a16763a703ab4f8234c8d3f71514c2
kjanka <kjankakrisztina@gmail.com>
2019-03-07 11:27:59 +0100

    update animal
    majd folytatom

--- 3-analysis-modell/java/src/main/java/hu/johndoe/spl/ass3/Animal.java
+++ 3-analysis-modell/java/src/main/java/hu/johndoe/spl/ass3/Animal.java
@@ -2,58 +2,25 @@ package hu.johndoe.spl.ass3;
 
 public abstract class Animal implements Updatable {
 
-    public Tile standingOn;
-    public Animal leaderAnimal;
-    public Animal guidedAnimal;
+   private Tile standingOn;
+   private Animal leaderAnimal;
+   private Animal guidedAnimal;
 
-    public void collideWithPanda (Animal panda) {
+   public void collideWithAnimal(Animal animal){}
+   public void collideWithPanda(Animal panda){}
+   public void collideWithOrangutan(Animal orangutan){}
+   public void kill(){}
+   public void startLeading(Animal leader){}
+   public void stopLeading(){}
+   public void moveTo(Tile targetTile){}
+   public void use(Item item){}
+   public void update(){}
+   public void goToSleep(Tile where){}
+   public void jump(){}
+   public void scare(){}
+   public void leadOut(){}
 
-    }
 
-    public void collideWithOrangutan (Animal orangutan) {
 
-    }
-
-    // TODO: Missing from DOCUMENT.md
-    public abstract void collideWithAnimal (Animal animal);
-
-    public void kill () {
-
-    }
-
-    public void startLeading (Animal leader) {
-
-        Animal previouslyGuidedAnimal = leader.guidedAnimal;
-
-        this.leaderAnimal = leader;
-        previouslyGuidedAnimal.leaderAnimal = this;
-        leader.guidedAnimal = this;
-
-    }
-
-    public void stopLeading () {
-
-        if (this.guidedAnimal != null) {
-            this.guidedAnimal.stopLeading ();
-        }
-
-        this.leaderAnimal = null;
-        this.guidedAnimal = null;
-
-    }
-
-    // TODO: Missing from DOCUMENT.md
-    public void moveTo (Tile targetTile) {
-
-        targetTile.accept (this);
-
-    }
-
-    // TODO: Missing from DOCUMENT.md
-    public void use (Item item) {
-
-        item.use (this);
-
-    }
 
 }
