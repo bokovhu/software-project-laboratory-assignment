cca98f0b337fbad232141950f16617aa6d6386e4
Botond János Kovács <botondjanoskovacs@gmail.com>
2019-03-23 13:32:45 +0100

    Command line interface done

--- panda-skeleton/src/main/java/hu/johndoe/panda/skeleton/Main.java
+++ panda-skeleton/src/main/java/hu/johndoe/panda/skeleton/Main.java
@@ -3,29 +3,77 @@ package hu.johndoe.panda.skeleton;
 import hu.johndoe.panda.skeleton.model._internal.ActionLogger;
 import hu.johndoe.panda.skeleton.scenario.TestScenario;
 
+import java.util.Scanner;
+
 public class Main {
 
+
+
     public static void main (String[] args) {
+
         System.out.println ("Welcome to John Doe's skeleton!");
-        System.out.println ("Please choose a scenario id and enter it: ");
+        System.out.println ("Type 'exit' to exit from the application");
+        System.out.println ("Type the number of the scenario to run it");
 
-        for (TestScenario testScenario : TestScenario.SCENARIOS) {
+        Scanner inputScanner = new Scanner (System.in);
 
-            System.out.println ("id: " + TestScenario.SCENARIOS.indexOf (testScenario) + " name:" + testScenario.getName ());
+        while (true) {
+
+            System.out.println ("~~~ Available scenarios ~~~");
+
+            for (int i = 0; i < TestScenario.SCENARIOS.size (); i++) {
+
+                TestScenario scenario = TestScenario.SCENARIOS.get (i);
+                System.out.println (
+                        String.format ("%d: %s", (i + 1), scenario.getName ())
+                );
+
+            }
+
+            System.out.println (
+                    String.format ("Type the number of the scenario to run! [1-%d]: ", TestScenario.SCENARIOS.size ())
+            );
+            String command = inputScanner.nextLine ();
+
+            if ("exit".equalsIgnoreCase (command)) {
+                System.out.println ("Thank you for using our application!");
+                System.exit (0);
+            }
+
+            Integer scenarioIndex = null;
+
+            while (true) {
+
+                if (command.matches ("[0-9]+")) {
+
+                    scenarioIndex = Integer.parseInt (command);
+                    if (scenarioIndex >= 1 && scenarioIndex <= TestScenario.SCENARIOS.size ()) {
+                        break;
+                    }
+
+                }
+
+                if ("exit".equalsIgnoreCase (command)) {
+                    System.out.println ("Thank you for using our application!");
+                    System.exit (0);
+                }
+
+                System.out.println (
+                        String.format ("Please type a number in the range [1-%d]: ", TestScenario.SCENARIOS.size ())
+                );
+                command = inputScanner.nextLine ();
+
+            }
+
+            TestScenario scenarioToRun = TestScenario.SCENARIOS.get (scenarioIndex);
+            System.out.println (
+                    String.format ("~~~ Running '%s' ~~~", scenarioToRun.getName ())
+            );
+
             ActionLogger.reset ();
-            //testScenario.getScenarioAction ().run ();
+            scenarioToRun.getScenarioAction ().run ();
 
         }
-
-        
-
-       /* for (TestScenario testScenario : TestScenario.SCENARIOS) {
-
-            System.out.println ("====== " + testScenario.getName () + " ======");
-            ActionLogger.reset ();
-            testScenario.getScenarioAction ().run ();
-
-        }*/
 
     }
 
